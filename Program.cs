3329e4b1a38703912c34b1dcf57fc769cad4c667
«Vlad» <fomichfomic@gmail.com>
2022-12-23 17:22:47 +0300

    code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив строк через запятую");
+string[] wordsMatrix;
+string words = Console.ReadLine();
+words = words.Replace(" ", "");
+wordsMatrix = words.Split(',');
+var result = new string[wordsMatrix.Length];
+var size = 0;
+foreach (var value in wordsMatrix )
+{
+    if (value.Length <= 3)
+    {
+        result[size] = value;
+        size++;
+    }
+}
+Console.WriteLine("Новый массив строк со значениями не более трёх символов:");
+Console.WriteLine(string.Join(", ", result, 0,  size));
